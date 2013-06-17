810be6a5c638910bb8455c34e315aa3750a6698b
kidovate <kidovate@gmail.com>
2013-06-16 20:10:00 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+meteor-nivo
+==========
+
+This package is a wrapper for the glorious [Nivo Slider](http://dev7studios.com/nivo-slider/).
+
+Please see the site for a tutorial on how to use the jQuery plugin.
---
+++ package.js
@@ -0,0 +1,29 @@
+Package.describe({
+    summary: "Nivo slider packaged for meteor."
+});
+
+Package.on_use(function (api) {
+    api.use('jquery', 'client');
+    api.add_files([
+        'lib/jquery.nivo.slider.js',
+        'lib/nivo-slider.css',
+        'lib/themes/bar/bar.css',
+        'lib/themes/bar/arrows.png',
+        'lib/themes/bar/bullets.png',
+        'lib/themes/bar/loading.gif',
+        'lib/themes/dark/dark.css',
+        'lib/themes/dark/arrows.png',
+        'lib/themes/dark/bullets.png',
+        'lib/themes/dark/loading.gif',
+        'lib/themes/default/default.css',
+        'lib/themes/default/arrows.png',
+        'lib/themes/default/bullets.png',
+        'lib/themes/default/loading.gif',
+        'lib/themes/light/light.css',
+        'lib/themes/light/arrows.png',
+        'lib/themes/light/bullets.png',
+        'lib/themes/light/loading.gif'
+    ], 'client'
+    );
+
+});
